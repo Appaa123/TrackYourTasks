d8da4cae08c146c0042a61c4d7ee97a3ffe010cc
Venkata Appaa Rao Komaragiri <appaarao22@gmail.com>
2025-09-21 22:19:32 +0530

    Updated images and removed counter

--- TrackYourTasks/MainPage.xaml.cs
+++ TrackYourTasks/MainPage.xaml.cs
@@ -12,18 +12,6 @@ namespace TrackYourTasks
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
         private async void OnTaskButtonClicked(object? sender, EventArgs e)
         {
             await Navigation.PushAsync(new TasksPage());
